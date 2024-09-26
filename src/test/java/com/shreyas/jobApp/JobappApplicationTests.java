82bed9147a03e8b29252fa3c735ce73f8c17b31c
shreyasda <shreyasshrivatsa@gmail.com>
2024-09-27 01:25:45 +0530

    Setup the project and created basic GET and POST requests

---
+++ src/test/java/com/shreyas/jobApp/JobappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreyas.jobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
